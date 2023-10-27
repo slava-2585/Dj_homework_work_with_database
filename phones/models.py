019932b5a167832bf7bce1beab0253991d6f5714
Nikiforov.Vyacheslav <slava-2585@yandex.ru>
2023-10-27 17:34:38 +0300

    models

--- phones/models.py
+++ phones/models.py
@@ -2,5 +2,10 @@ from django.db import models
 
 
 class Phone(models.Model):
-    # TODO: Добавьте требуемые поля
-    pass
+    name = models.TextField()
+    price = models.FloatField()
+    image = models.FilePathField()
+    release_date = models.DateField()
+    lte_exists = models.BooleanField()
+    slug = models.SlugField()
+
